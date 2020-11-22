ebb93dd4e9f20b0f7e9915b29ae20d8b0e196d13
orhan <orhanu2020@gmail.com>
2020-11-22 16:54:47 -0500

    added testng framework for test automation

--- java/FullTest/src/test/java/org/cb/ta/GoogleSearchTest.java
+++ java/FullTest/src/test/java/org/cb/ta/GoogleSearchTest.java
@@ -1,20 +1,32 @@
 package org.cb.ta;
 
-import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.google.inject.Key;
 
 public class GoogleSearchTest extends BaseTest {
+	
 	@Test
-	public void gotoGoogleAndEnterSearchText() {
+	public void gotoGoogleAndEnterSearchText() throws InterruptedException {
 		
 		GoogleSearchMainPage googleSearchMainPage = new GoogleSearchMainPage(driver);
 		googleSearchMainPage.go();
 		WebElement searchTextField = googleSearchMainPage.getSearchText();
 		searchTextField.sendKeys("codingbook");
+		searchTextField.sendKeys(Keys.RETURN);
+		
     	//test
-    	//if search file has text codinbook
+		System.out.println("******" + searchTextField.getAttribute("value"));
+    	assertEquals("Invalid text in search field", 
+    			"codingbook", 
+    			searchTextField.getAttribute("value"));
 	}
 }
