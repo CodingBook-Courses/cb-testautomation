1eb1bc377372513543acd59a5dcd9ffb95ef8884
orhan <orhanu2020@gmail.com>
2020-12-07 08:46:59 -0500

    Driver close operation is removed from here. It was causing error that I could not solve it

--- java/FullTest/src/test/java/org/cb/ta/BaseTest.java
+++ java/FullTest/src/test/java/org/cb/ta/BaseTest.java
@@ -2,23 +2,21 @@ package org.cb.ta;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.*;
 
 public class BaseTest {
+
 	protected static WebDriver driver;
-	protected BasePage basePage;
-	
+
 	static {
 		System.setProperty("webdriver.chrome.driver", "webdrivers\\chromedriver.exe");
     	driver = new ChromeDriver();
 	}
 
+	protected BasePage basePage;
+
 	public BaseTest(BasePage basePage) {
 		this.basePage = basePage;
-	}
-
-	@AfterMethod
-	public void tearDown() {
-		driver.close();
 	}
 }
