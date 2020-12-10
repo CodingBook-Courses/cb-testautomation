b0c124b8ae5dadf95e9a3c9afcc29e4bc849d968
orhan <orhanu2020@gmail.com>
2020-12-10 15:35:46 -0500

    Multiple xml configuration files

--- java/FullTest/src/main/java/org/cb/ta/_10_TestNG/session.md
+++ java/FullTest/src/main/java/org/cb/ta/_10_TestNG/session.md
@@ -5,6 +5,7 @@
 4. @Test Annotation
 5. testng.xml
    - suite -> test > class/..
+6. Multiple xml configuration files
    
 ### NOTES
 - If you de not define an order, test cases are ordered alphabetically 
---
+++ java/FullTest/src/test/java/org/cb/ta/_10_TestNG/EmployeeSalaryTest.java
@@ -0,0 +1,10 @@
+package org.cb.ta._10_TestNG;
+
+import org.testng.annotations.Test;
+
+public class EmployeeSalaryTest {
+    @Test
+    public void EmployeeSalaryTest() {
+        System.out.println("EmployeeSalaryTest");
+    }
+}
