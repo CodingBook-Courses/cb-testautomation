3d3518f18919ea098f9a60184250864995424037
orhan <orhanu2020@gmail.com>
2020-12-10 19:41:26 -0500

    Defining test scoped parameter

--- java/FullTest/src/test/java/org/cb/ta/_10_TestNG/EmployeeSalaryTest.java
+++ java/FullTest/src/test/java/org/cb/ta/_10_TestNG/EmployeeSalaryTest.java
@@ -1,10 +1,13 @@
 package org.cb.ta._10_TestNG;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class EmployeeSalaryTest {
     @Test
-    public void EmployeeSalaryTest() {
-        System.out.println("EmployeeSalaryTest");
+    @Parameters({"url"})
+    public void EmployeeSalaryTest(String url)
+    {
+        System.out.println(url);
     }
 }
